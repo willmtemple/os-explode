0dc590ed49ea5c126103ab89584129a833a1ece3
14rcole <rcyoalne@gmail.com>
2016-07-25 14:27:54 -0400

    add files to add pods on kubernetes

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM openshift/origin-base
+LABEL Description="This image is used to watch for changes in a registry explode new images onto a persistent volume" Version="0.1"
+
+ADD ./exploder /
+
+# EXPOSE ?
+
+ENTRYPOINT ["/exploder"]
---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# Build the docker image
+docker build -t os-exploder .
+
+# start kubernetes service account
+kubectl create -f os-exploder-sa.yaml
+
+# start k8s pod with the api token for the above service account
+kubectl create -f os-exploder-pod.yaml
+
+oadm policy add-scc-to-user privileged system:serviceaccounts:default:os-exploder
