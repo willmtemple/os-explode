c868660f7774a23c5427c5b034c2db294921a9d6
14rcole <rcyoalne@gmail.com>
2016-07-25 14:27:54 -0400

    Files for deployment on OpenShift

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM fedora:24
+=======
+LABEL Description="This image is used to watch for changes in a registry explode new images onto a persistent volume" Version="0.1"
+
+ADD ./os-exploder /
+
+VOLUME /registry
+VOLUME /explode
+
+RUN dnf install -y ostree
+
+ENTRYPOINT ["/os-exploder"]
---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# Build the docker image
+
+docker build -t exploder .
+
+# start kubernetes service account
+oc create -f os-exploder-serviceaccount.yaml
+
+# start k8s pod with the api token for the above service account
+oc create -f os-exploder-deploymentconfig.yaml
+
+oadm policy add-scc-to-user privileged system:serviceaccount:default:exploder
