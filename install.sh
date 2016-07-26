7069fe2f83fd108fca4b0310ca09675c13cdb27d
14rcole <rcyoalne@gmail.com>
2016-07-26 13:50:14 -0400

    Files for deployment on OpenShift

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # Build the docker image
-docker build -t os-exploder .
+
+docker build -t exploder .
 
 # start kubernetes service account
 oc create -f os-exploder-serviceaccount.yaml
@@ -11,4 +12,4 @@ oc create -f os-exploder-serviceaccount.yaml
 # start k8s pod with the api token for the above service account
 oc create -f os-exploder-deploymentconfig.yaml
 
-oadm policy add-scc-to-user privileged system:serviceaccounts:default:os-exploder
+oadm policy add-scc-to-user privileged system:serviceaccount:default:exploder
